5616ae36dfb879b79fadc43160c935e0c5c6e263
Martin Hansen <martin@martinhansen.no>
2023-02-17 16:06:11 +0100

    Add documentation for relative json pointer util

--- src/relative-json-pointer.js
+++ src/relative-json-pointer.js
@@ -1,7 +1,15 @@
 import pointer from 'json-pointer';
 
-function resolveRelativeJsonPointer(data, base, relative) {
-  var tokens = pointer.parse(base);
+/**
+ * Resolves a relative json pointer to a value in the provided data. Needs a base pointer to resolve from.
+ *
+ * @param {*} data the json data
+ * @param {*} basePointer the starting point for the relative pointer
+ * @param {*} relative the relative pointer
+ * @returns the value located at the relative pointer location
+ */
+function resolveRelativeJsonPointer(data, basePointer, relative) {
+  var tokens = pointer.parse(basePointer);
   var parts = /^([\d]+)(.+)?/.exec(relative);
   var prefix = parseInt(parts[1], 10);
   var relPointer = parts[2];
